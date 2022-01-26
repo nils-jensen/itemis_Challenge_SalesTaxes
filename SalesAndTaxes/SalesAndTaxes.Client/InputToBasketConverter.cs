364a33377a8b4acae7c7987d506bfea879286999
nils-jensen <nilsjensendev@gmail.com>
2022-01-26 14:36:54 +0100

    Tests for #5

--- SalesAndTaxes/SalesAndTaxes.Client/InputToBasketConverter.cs
+++ SalesAndTaxes/SalesAndTaxes.Client/InputToBasketConverter.cs
@@ -6,6 +6,9 @@ namespace SalesAndTaxes.Client
 {
     public class InputToBasketConverter
     {
-       
+        public string Convert()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SalesAndTaxes/SalesAndTaxes.Tests/ClientTests/InputToBasketConverterTests.cs
+++ SalesAndTaxes/SalesAndTaxes.Tests/ClientTests/InputToBasketConverterTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalesAndTaxes.Client;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,76 @@ namespace SalesAndTaxes.Tests.ClientTests
     [TestClass]
     public class InputToBasketConverterTests
     {
-       
+        /// <summary>
+        /// Basket is: <br />
+        /// 1 book at 12.49 <br />
+        /// 1 music CD at 14.99 <br />
+        /// 1 chocolate bar at 0.85 <br />
+        /// </summary>
+        [TestMethod]
+        public void FirstBasket()
+        {
+            var expectedOutput = new StringBuilder()
+                                 .AppendLine("1 book: 12.49")
+                                 .AppendLine("1 music CD: 16.49")
+                                 .AppendLine("1 chocolate bar: 0.85")
+                                 .AppendLine("Sales Taxes: 1.50")
+                                 .AppendLine("Total: 29.83")
+                                 .ToString();
+
+            var converter = new InputToBasketConverter();
+
+            Assert.IsTrue(
+                string.Equals(expectedOutput, converter.Convert())
+            );
+        }
+
+        /// <summary>
+        /// Basket is: <br />
+        /// 1 imported box of chocolates at 10.00 <br />
+        /// 1 imported bottle of perfume at 47.50 <br />
+        /// </summary>
+        [TestMethod]
+        public void SecondBasket()
+        {
+            var expectedOutput = new StringBuilder()
+                                 .AppendLine("1 imported box of chocolates: 10.50")
+                                 .AppendLine("1 imported bottle of perfume: 54.65")
+                                 .AppendLine("Sales Taxes: 7.65")
+                                 .AppendLine("Total: 65.15")
+                                 .ToString();
+
+            var converter = new InputToBasketConverter();
+
+            Assert.IsTrue(
+                string.Equals(expectedOutput, converter.Convert())
+            );
+        }
+
+        /// <summary>
+        /// Basket is: <br />
+        /// 1 imported bottle of perfume at 27.99 <br />
+        /// 1 bottle of perfume at 18.99 <br />
+        /// 1 packet of headache pills at 9.75 <br />
+        /// 1 box of imported chocolates at 11.25 <br />
+        /// </summary>
+        [TestMethod]
+        public void ThirdBasket()
+        {
+            var expectedOutput = new StringBuilder()
+                                 .AppendLine("1 imported bottle of perfume: 32.19")
+                                 .AppendLine("1 bottle of perfume: 20.89")
+                                 .AppendLine("1 packet of headache pills: 9.75")
+                                 .AppendLine("1 imported box of chocolates: 11.85")
+                                 .AppendLine("Sales Taxes: 6.70")
+                                 .AppendLine("Total: 74.68")
+                                 .ToString();
+
+            var converter = new InputToBasketConverter();
+
+            Assert.IsTrue(
+                string.Equals(expectedOutput, converter.Convert())
+            );
+        }
     }
 }
