093dada9928c2331eab105951db697cbf6c5c31a
nils-jensen <nilsjensendev@gmail.com>
2022-01-27 12:49:25 +0100

    Tests for #5 refactored.

--- SalesAndTaxes/SalesAndTaxes.Tests/ClientTests/InputToBasketConverterTests.cs
+++ SalesAndTaxes/SalesAndTaxes.Tests/ClientTests/InputToBasketConverterTests.cs
@@ -26,10 +26,19 @@ namespace SalesAndTaxes.Tests.ClientTests
                                  .AppendLine("Total: 29.83")
                                  .ToString();
 
-            var converter = new InputToBasketConverter();
+            var input = new string[]
+            {
+                "1 book at 12.49",
+                "1 music CD at 14.99",
+                "1 chocolate bar at 0.85",
+            };
+
+            var converter = new InputToBasketConverter(input);
+
+            var convertedBasket = converter.Convert();
 
             Assert.IsTrue(
-                string.Equals(expectedOutput, converter.Convert())
+                string.Equals(expectedOutput, convertedBasket)
             );
         }
 
@@ -48,10 +57,20 @@ namespace SalesAndTaxes.Tests.ClientTests
                                  .AppendLine("Total: 65.15")
                                  .ToString();
 
-            var converter = new InputToBasketConverter();
+            var input = new string[]
+            {
+                "1 imported box of chocolates at 10.00",
+                "1 imported bottle of perfume at 47.50",
+            };
 
+            var converter = new InputToBasketConverter(input);
+
+            var convertedBasket = converter.Convert();
+
+
+            // fails as there are 2 cents missing from the total
             Assert.IsTrue(
-                string.Equals(expectedOutput, converter.Convert())
+                string.Equals(expectedOutput, convertedBasket)
             );
         }
 
@@ -74,10 +93,21 @@ namespace SalesAndTaxes.Tests.ClientTests
                                  .AppendLine("Total: 74.68")
                                  .ToString();
 
-            var converter = new InputToBasketConverter();
+            var input = new string[]
+            {
+                "1 imported bottle of perfume at 27.99",
+                "1 bottle of perfume at 18.99",
+                "1 packet of headache pills at 9.75",
+                "1 box of imported chocolates at 11.25",
+            };
 
+            var converter = new InputToBasketConverter(input);
+
+            var convertedBasket = converter.Convert();
+
+            // fails with 5 cents missing from taxes and 4 from the total.
             Assert.IsTrue(
-                string.Equals(expectedOutput, converter.Convert())
+                string.Equals(expectedOutput, convertedBasket)
             );
         }
     }
